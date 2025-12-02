589b0cf39fe9910874dc1a4715c5139a43e0aab5
Mahmoud <mahmoud.355.mhm@gmail.com>
2025-12-02 19:33:23 +0200

    variable.tf

--- varaible.tf
+++ varaible.tf
@@ -1,41 +1,40 @@
 variable "AWS_REGION" {
-    type        = string
-    default     = "us-east-2"
+  type    = string
+  default = "us-east-2"
 }
 
-variable "LEVELUP_VPC_CIDR_BLOC" {
-  description = "The CIDR block for the VPC"
+variable "BLOGGING_VPC_CIDR_BLOCK" {
+  description = "CIDR block for the Blogging VPC"
   type        = string
   default     = "10.0.0.0/16"
 }
 
-variable "LEVELUP_VPC_PUBLIC_SUBNET1_CIDR_BLOCK" {
-  description = "The CIDR block for the VPC"
+variable "BLOGGING_VPC_PUBLIC_SUBNET1_CIDR_BLOCK" {
+  description = "CIDR block for Public Subnet 1"
   type        = string
   default     = "10.0.101.0/24"
 }
 
-variable "LEVELUP_VPC_PUBLIC_SUBNET2_CIDR_BLOCK" {
-  description = "The CIDR block for the VPC"
+variable "BLOGGING_VPC_PUBLIC_SUBNET2_CIDR_BLOCK" {
+  description = "CIDR block for Public Subnet 2"
   type        = string
   default     = "10.0.102.0/24"
 }
 
-variable "LEVELUP_VPC_PRIVATE_SUBNET1_CIDR_BLOCK" {
-  description = "The CIDR block for the VPC"
+variable "BLOGGING_VPC_PRIVATE_SUBNET1_CIDR_BLOCK" {
+  description = "CIDR block for Private Subnet 1"
   type        = string
   default     = "10.0.1.0/24"
 }
 
-variable "LEVELUP_VPC_PRIVATE_SUBNET2_CIDR_BLOCK" {
-  description = "The CIDR block for the VPC"
+variable "BLOGGING_VPC_PRIVATE_SUBNET2_CIDR_BLOCK" {
+  description = "CIDR block for Private Subnet 2"
   type        = string
   default     = "10.0.2.0/24"
 }
 
 variable "ENVIRONMENT" {
-  description = "AWS VPC Environment Name"
+  description = "Environment name (e.g. Development, Staging, Production)"
   type        = string
   default     = "Development"
 }
-
