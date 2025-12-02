ffba55d7089d73f9d9c666f9dcdf4b85130dc250
Mahmoud <mahmoud.355.mhm@gmail.com>
2025-12-02 19:44:56 +0200

    eks

--- ekscluster.tf
+++ ekscluster.tf
@@ -1,40 +1,53 @@
-
-
+# -------------------------------------------------------
+# EKS Cluster
+# -------------------------------------------------------
 resource "aws_eks_cluster" "aws_eks" {
-  name     = "eks_cluster_levelup"
+  name     = "${var.ENVIRONMENT}-blogging-eks-cluster"
   role_arn = aws_iam_role.eks_cluster.arn
 
   vpc_config {
-    subnet_ids = module.vpc.public_subnets
+    subnet_ids = concat(
+      module.vpc.public_subnets,
+      module.vpc.private_subnets
+    )
   }
 
   depends_on = [
-    aws_iam_role_policy_attachment.AmazonEKSClusterPolicy,  
+    aws_iam_role_policy_attachment.AmazonEKSClusterPolicy,
     aws_iam_role_policy_attachment.AmazonEKSServicePolicy,
   ]
 
   tags = {
-    Name = "EKS_Cluster_LevelUp"
+    Name        = "${var.ENVIRONMENT}-blogging-eks-cluster"
+    Environment = var.ENVIRONMENT
   }
 }
 
+# -------------------------------------------------------
+# EKS Managed Node Group
+# -------------------------------------------------------
 resource "aws_eks_node_group" "node" {
   cluster_name    = aws_eks_cluster.aws_eks.name
-  node_group_name = "node_levelup"
+  node_group_name = "${var.ENVIRONMENT}-blogging-nodegroup"
   node_role_arn   = aws_iam_role.eks_nodes.arn
-  subnet_ids      = module.vpc.public_subnets
+
+  # Worker nodes MUST be in private subnets
+  subnet_ids = module.vpc.private_subnets
 
   scaling_config {
     desired_size = 1
-    max_size     = 1
+    max_size     = 2
     min_size     = 1
   }
 
-  # Ensure that IAM Role permissions are created before and deleted after EKS Node Group handling.
-  # Otherwise, EKS will not be able to properly delete EC2 Instances and Elastic Network Interfaces.
   depends_on = [
     aws_iam_role_policy_attachment.AmazonEKSWorkerNodePolicy,
     aws_iam_role_policy_attachment.AmazonEKS_CNI_Policy,
     aws_iam_role_policy_attachment.AmazonEC2ContainerRegistryReadOnly,
   ]
+
+  tags = {
+    Name        = "${var.ENVIRONMENT}-blogging-nodegroup"
+    Environment = var.ENVIRONMENT
+  }
 }
