322a0440cae7c204413de936b01f49505ac48ab4
Mahmoud <mahmoud.355.mhm@gmail.com>
2025-12-02 19:53:50 +0200

    Refactor EKS cluster and node group resources

--- ekscluster.tf
+++ ekscluster.tf
@@ -1,53 +1,82 @@
-# -------------------------------------------------------
-# EKS Cluster
-# -------------------------------------------------------
-resource "aws_eks_cluster" "aws_eks" {
-  name     = "${var.ENVIRONMENT}-blogging-eks-cluster"
+resource "aws_eks_cluster" "blogging_eks" {
+  name     = "${var.environment}-blogging-eks-cluster"
   role_arn = aws_iam_role.eks_cluster.arn
 
   vpc_config {
     subnet_ids = concat(
-      module.vpc.public_subnets,
-      module.vpc.private_subnets
+      aws_subnet.public_1.id != "" ? [aws_subnet.public_1.id] : [],
+      aws_subnet.public_2.id != "" ? [aws_subnet.public_2.id] : [],
+      aws_subnet.private_1.id != "" ? [aws_subnet.private_1.id] : [],
+      aws_subnet.private_2.id != "" ? [aws_subnet.private_2.id] : []
     )
+    endpoint_private_access = true
+    endpoint_public_access  = true
   }
 
   depends_on = [
-    aws_iam_role_policy_attachment.AmazonEKSClusterPolicy,
-    aws_iam_role_policy_attachment.AmazonEKSServicePolicy,
+    aws_iam_role_policy_attachment.cluster_policy,
+    aws_iam_role_policy_attachment.service_policy,
   ]
 
   tags = {
-    Name        = "${var.ENVIRONMENT}-blogging-eks-cluster"
-    Environment = var.ENVIRONMENT
+    Name        = "${var.environment}-blogging-eks-cluster"
+    Environment = var.environment
   }
 }
 
-# -------------------------------------------------------
-# EKS Managed Node Group
-# -------------------------------------------------------
-resource "aws_eks_node_group" "node" {
-  cluster_name    = aws_eks_cluster.aws_eks.name
-  node_group_name = "${var.ENVIRONMENT}-blogging-nodegroup"
+resource "aws_eks_node_group" "blogging_nodegroup" {
+  cluster_name    = aws_eks_cluster.blogging_eks.name
+  node_group_name = "${var.environment}-blogging-nodegroup"
   node_role_arn   = aws_iam_role.eks_nodes.arn
 
-  # Worker nodes MUST be in private subnets
-  subnet_ids = module.vpc.private_subnets
+  subnet_ids = [
+    aws_subnet.private_1.id,
+    aws_subnet.private_2.id,
+  ]
 
   scaling_config {
-    desired_size = 1
-    max_size     = 2
-    min_size     = 1
+    desired_size = var.node_group_desired_size
+    max_size     = var.node_group_max_size
+    min_size     = var.node_group_min_size
   }
 
   depends_on = [
-    aws_iam_role_policy_attachment.AmazonEKSWorkerNodePolicy,
-    aws_iam_role_policy_attachment.AmazonEKS_CNI_Policy,
-    aws_iam_role_policy_attachment.AmazonEC2ContainerRegistryReadOnly,
+    aws_iam_role_policy_attachment.worker_node_policy,
+    aws_iam_role_policy_attachment.cni_policy,
+    aws_iam_role_policy_attachment.ecr_readonly,
   ]
 
   tags = {
-    Name        = "${var.ENVIRONMENT}-blogging-nodegroup"
-    Environment = var.ENVIRONMENT
+    Name        = "${var.environment}-blogging-nodegroup"
+    Environment = var.environment
   }
+}
+
+# OIDC provider: required for IRSA (used later)
+data "aws_eks_cluster" "cluster" {
+  name = aws_eks_cluster.blogging_eks.name
+  depends_on = [aws_eks_cluster.blogging_eks]
+}
+
+data "aws_eks_cluster_auth" "cluster" {
+  name = aws_eks_cluster.blogging_eks.name
+}
+
+resource "aws_iam_openid_connect_provider" "eks_oidc" {
+  client_id_list  = ["sts.amazonaws.com"]
+  thumbprint_list = [data.tls_certificate.eks_issuer.certificates[0].sha1_fingerprint]
+  url             = data.aws_eks_cluster.cluster.identity[0].oidc[0].issuer
+}
+
+# helper to fetch OIDC thumbprint
+data "tls_certificate" "eks_issuer" {
+  url = replace(data.aws_eks_cluster.cluster.identity[0].oidc[0].issuer, "https://", "")
+}
+
+output "cluster_endpoint" {
+  value = aws_eks_cluster.blogging_eks.endpoint
+}
+
+output "cluster_certificate_authority_data" {
+  value = aws_eks_cluster.blogging_eks.certificate_authority[0].data
 }
